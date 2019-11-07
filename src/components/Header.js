fefe8328aa8261c1dd761f0c4bbb8add8f2fd531
Julien <j_dommel@yahoo.fr>
2019-11-07 15:17:12 +0100

    new Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/frizmee-logo.png";
 
 function Header(props) {
   return (
     <header>
-      <h1>Frizmee !!!</h1>
+      <img src={logo} alt="frizmee-logo" className="frizmee-logo" />
+      {/* <h1>Frizmee !!!</h1> */}
       {props.page === "Home" && (
         <Link to="/friztip">
           <i className="far fa-lightbulb fa-4x link-icon"></i>
